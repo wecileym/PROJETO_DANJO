cbc9351cd6ea35d7cc039aaeab936ee5e8ff3148
Wecileym <marianoweciley@gmail.com>
2024-11-26 10:57:11 -0300

    body project

--- projeto/recipies/views.py
+++ projeto/recipies/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    return render(request, 'recipies/home.html')
+    return render(request, 'recipies/pages/home.html')
 
 def contato(request):
     return HttpResponse("contato")
