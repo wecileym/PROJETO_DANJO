8f121fac9b1e7987f2cedd95a111d1409628e7af
Wecileym <marianoweciley@gmail.com>
2024-12-03 09:44:28 -0300

    modificado projeto e recipies

--- projeto/projeto/urls.py
+++ projeto/projeto/urls.py
@@ -1,3 +1,5 @@
+
+
 """
 URL configuration for projeto project.
 
@@ -16,7 +18,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
@@ -24,3 +27,7 @@ urlpatterns = [
     path('', include('recipies.urls')),
     path('recipies/', include('recipies.urls'))
 ]
+
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- projeto/recipies/models.py
+++ projeto/recipies/models.py
@@ -5,6 +5,9 @@ from django.contrib.auth.models import User
 class Category(models.Model):
     name = models.CharField(max_length=65)
 
+    def __str__(self):
+        return self.name
+
 
 # Create your models here.
 class Recipe(models.Model):
@@ -27,6 +30,10 @@ class Recipe(models.Model):
     author = models.ForeignKey(
         User, on_delete=models.SET_NULL, null=True
     )
+
+
+    def __str__(self):
+        return self.title
 
 # EDITED
 # title description slug
